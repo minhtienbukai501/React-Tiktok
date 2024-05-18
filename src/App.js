bfcefd92047018efd36fb6b46da20d9bd0f462c4
Le Minh Tien <minhtienbukai501@gmail.com>
2024-05-18 22:57:42 +0700

    bai60

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Button from './components/button';
 
-
 function App() {
-  return (
-    <div className="App">
-      <Button></Button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button></Button>
+        </div>
+    );
 }
 
 export default App;
